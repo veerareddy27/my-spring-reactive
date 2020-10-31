7f72e8a008f3660cefd7d999bdfd8154c3f5034a
Veera Reddy <veerareddy27@users.noreply.github.com>
2020-10-31 10:12:08 -0400

    my-spring-reactive

---
+++ src/main/java/com/thd/myspringreactive/MySpringReactiveApplication.java
@@ -0,0 +1,13 @@
+package com.thd.myspringreactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringReactiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySpringReactiveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/thd/myspringreactive/MySpringReactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thd.myspringreactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringReactiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
